82b92245740bae47e00ad8027e66b22fc6923770
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-01 14:15:55 +0800

    >  compile NEMU 20192160000 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:15:55 up 48 min,  1 user,  load average: 0.41, 0.50, 0.50 5678048ecbef420e7c1b693a99d65ef11c697781

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -36,6 +36,13 @@ static int cmd_q(char *args) {
 	return -1;
 }
 
+static int cmd_si(char *args) {
+	printf("%s\n", args);
+	if(args == NULL) cpu_exec(1);
+	else cpu_exec(atoi(args));
+	return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -46,6 +53,7 @@ static struct {
 	{ "help", "Display informations about all supported commands", cmd_help },
 	{ "c", "Continue the execution of the program", cmd_c },
 	{ "q", "Exit NEMU", cmd_q },
+	{ "si", "Continue the execution of the program in N steps", cmd_si },
 
 	/* TODO: Add more commands */
 
