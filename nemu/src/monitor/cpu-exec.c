3720a37eb3e65b8dba65b148c9173a568d2f981b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-01 15:09:10 +0800

    >  compile NEMU 20192160000 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:09:10 up  1:41,  1 user,  load average: 1.39, 0.88, 0.78 a8b9d256bf726611123fe3e09ced1546122fac1b

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -7,7 +7,7 @@
  * This is useful when you use the `si' command.
  * You can modify this value as you want.
  */
-#define MAX_INSTR_TO_PRINT 10
+#define MAX_INSTR_TO_PRINT 50
 
 int nemu_state = STOP;
 
