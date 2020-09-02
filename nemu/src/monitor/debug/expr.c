8717cb9b723f41ae64fb529bed1ad8c78a9e78d5
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-02 12:58:33 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:58:33 up 50 min,  1 user,  load average: 0.60, 0.40, 0.28 41242a57678f4f37812a628de7e2186c434dc5d2

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -181,7 +181,7 @@ uint32_t eval(int l, int r, bool *success, int mod) {	//if mod=1, the first toke
 	if(!(*success))return *success = false;
 	printf("%d %d %d\n",l,now-1,a);
 	printf("%d %d %d\n",now+1,r,b);
-	printf("%d\n",tokens[now].type);
+	printf("%x\n",a/b);
 	if(tokens[now].type == PLUS) return a + b;
 	if(tokens[now].type == STAR) return a * b;
 	if(tokens[now].type == DIV) return a / b;	
