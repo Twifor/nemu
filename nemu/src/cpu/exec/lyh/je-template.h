cf7c7412b7c48898b70af2566d80cfd0de79b014
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 17:26:14 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:26:14 up  3:39,  1 user,  load average: 0.66, 0.87, 0.92 6bbc1ffbd2189c47d9d49c4db0af91a42f3ade4

--- nemu/src/cpu/exec/lyh/je-template.h
+++ nemu/src/cpu/exec/lyh/je-template.h
@@ -4,8 +4,8 @@
 
 make_helper(concat(je_i_, SUFFIX)) {
 	int len = concat(decode_i_, SUFFIX) (eip + 1);
-	print_asm("je 0x%x", op_src->imm + cpu.eip + len + 1);
-	if(cpu.ZF == 1) cpu.eip += op_src->imm;
+	print_asm("je 0x%x", (DATA_TYPE_S)op_src->imm + cpu.eip + len + 1);
+	if(cpu.ZF == 1) cpu.eip += (DATA_TYPE_S)op_src->imm;
 	return len + 1;
 }
 
--- nemu/src/cpu/exec/lyh/jmp-template.h
+++ nemu/src/cpu/exec/lyh/jmp-template.h
@@ -4,14 +4,14 @@
 
 make_helper(concat(jmp_i_, SUFFIX)) {
 	int len = concat(decode_i_, SUFFIX)(eip + 1);
-	cpu.eip += op_src->val;
+	cpu.eip += (DATA_TYPE_S)op_src->val;
 	print_asm("jmp 0x%x", cpu.eip + len + 1);
 	return len + 1;
 }
 
 make_helper(concat(jmp_rm_, SUFFIX)) {
 	int len = concat(decode_rm_, SUFFIX)(eip + 1);
-	cpu.eip += op_src->val;
+	cpu.eip += (DATA_TYPE_S)op_src->val;
 	print_asm("jmp %s", op_src->str);
 	return len + 1;
 }
