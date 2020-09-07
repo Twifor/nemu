e7808897636336811ead6604edf03efd3db15ea1
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 16:01:28 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:01:28 up  2:14,  1 user,  load average: 1.03, 0.77, 0.75 622eafcf79076fd3ddef491fea0263674eee530

--- nemu/src/cpu/exec/lyh/pop-template.h
+++ nemu/src/cpu/exec/lyh/pop-template.h
@@ -3,8 +3,7 @@
 #define instr pop
 
 void do_execute() {
-	printf("%s\n", REG_NAME(op_src->reg));
-	OPERAND_W(op_src, MEM_R(R_ESP));
+	OPERAND_W(op_src, MEM_R(reg_l(R_ESP)));
 	reg_l (R_ESP) += DATA_BYTE;
 	print_asm_template1();
 }
