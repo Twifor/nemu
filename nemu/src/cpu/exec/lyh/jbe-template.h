a5584cf98a88c516e75733dc1a273bcf498a1d86
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 17:24:40 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:24:40 up  3:37,  1 user,  load average: 1.19, 0.99, 0.96 21cface6bfc25e23b9677178f7f12edb126a688

--- nemu/src/cpu/exec/lyh/jbe-template.h
+++ nemu/src/cpu/exec/lyh/jbe-template.h
@@ -4,9 +4,8 @@
 
 make_helper(concat(jbe_i_, SUFFIX)) {
 	int len = concat(decode_i_, SUFFIX) (eip + 1);
-	printf("0x%x 0x%x 0x%x\n", op_src->imm, cpu.eip+len+1, op_src->imm+cpu.eip+len+1);
-	print_asm("jbe 0x%x", op_src->imm + cpu.eip + len + 1);
-	if(cpu.ZF == 1 || cpu.CF == 1) cpu.eip += op_src->imm;
+	print_asm("jbe 0x%x", (DATA_TYPE_S)op_src->imm + cpu.eip + len + 1);
+	if(cpu.ZF == 1 || cpu.CF == 1) cpu.eip += (DATA_TYPE_S)op_src->imm;
 	return len + 1;
 }
 
