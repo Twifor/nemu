bd267c52817abe81eee1b787bf93c33ed04d533b
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 21:27:42 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:27:42 up  7:40,  1 user,  load average: 0.96, 0.83, 0.69 ee7410f4daebbaeaea8631d3fc1d4898219b79e

--- nemu/src/cpu/exec/lyh/push-template.h
+++ nemu/src/cpu/exec/lyh/push-template.h
@@ -3,13 +3,19 @@
 #define instr push
 
 void do_execute() {
-	reg_l (R_ESP) -= DATA_BYTE;
-	MEM_W(reg_l(R_ESP), op_src->val);
+	if(DATA_BYTE == 2) {
+		reg_l(R_ESP) -= 2;
+		swaddr_write(reg_l(R_ESP), 2, op_src->val);
+	} else {
+		reg_l(R_ESP) -= 4;
+		swaddr_write(reg_l(R_ESP), 4, op_src->val);
+	}
 	print_asm_template1();
 }
-
+#if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(r)
 make_instr_helper(rm)
+#endif
 make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"
