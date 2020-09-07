55392c713e60dbed0e487bd301bb6a95d4a128a3
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 23:14:24 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:14:24 up  9:27,  1 user,  load average: 1.12, 0.90, 1.00 292e47b6a02c2d2b5404ee61ae43afa7c49758c1

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -65,12 +65,12 @@ make_group(group2_cl_v,
 /* 0xf6 */
 make_group(group3_b,
 	test_i2rm_b, inv, inv, inv, 
-	inv, imul_rm2a_b, inv, idiv_rm_b)
+	mul_rm_b, imul_rm2a_b, inv, idiv_rm_b)
 
 /* 0xf7 */
 make_group(group3_v,
 	test_i2rm_v, inv, inv, inv, 
-	inv, imul_rm2a_v, inv, idiv_rm_v)
+	mul_rm_v, imul_rm2a_v, inv, idiv_rm_v)
 
 /* 0xfe */
 make_group(group4,
