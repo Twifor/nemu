013cd09241e6d9beccf07afd981f36e2fbd0482f
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 23:25:33 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  23:25:33 up  9:38,  1 user,  load average: 0.46, 0.56, 0.79 9291642624ee024e74b2024d40eb87debfb6e09

--- nemu/src/cpu/exec/lyh/clt-template.h
+++ nemu/src/cpu/exec/lyh/clt-template.h
@@ -4,10 +4,10 @@
 
 make_helper(concat(clt_, SUFFIX)) {
 	if(DATA_BYTE == 2) {
-		if(cpu.gpr[0]._16 < 0) cpu.gpr[2]._16 = 0xffff;
+		if((int16_t)cpu.gpr[0]._16 < 0) cpu.gpr[2]._16 = 0xffff;
 		else cpu.gpr[2]._16 = 0;
 	} else if(DATA_BYTE == 4) { 
-		if(cpu.eax < 0) cpu.edx = 0xffffffff;
+		if((int32_t)cpu.eax < 0) cpu.edx = 0xffffffff;
 		else cpu.edx = 0;
 	}
 	print_asm("clt" str(SUFFIX));
