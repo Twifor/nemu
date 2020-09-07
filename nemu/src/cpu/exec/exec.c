a926973523c89cb713ab06ae1e3a42bcef76e6bd
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-07 18:07:51 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:07:51 up  4:21,  1 user,  load average: 1.68, 1.33, 1.18 180d71673d45358afb06fdf1f8f6f4b3561890ad

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -19,17 +19,17 @@ static make_helper(_2byte_esc);
 	
 /* 0x80 */
 make_group(group1_b,
-	add_i2rm_b, inv, adc_i2rm_b, inv, 
+	add_i2rm_b, or_i2rm_b, adc_i2rm_b, inv, 
 	and_i2rm_b, sub_i2rm_b, xor_i2rm_b, cmp_i2rm_b)
 
 /* 0x81 */
 make_group(group1_v,
-	add_i2rm_v, inv, adc_i2rm_v, inv, 
+	add_i2rm_v, or_i2rm_v, adc_i2rm_v, inv, 
 	and_i2rm_v, sub_i2rm_v, xor_i2rm_v, cmp_i2rm_v)
 
 /* 0x83 */
 make_group(group1_sx_v,
-	add_si2rm_v, inv, adc_si2rm_v, inv, 
+	add_si2rm_v, or_si2rm_v, adc_si2rm_v, inv, 
 	and_si2rm_v, sub_si2rm_v, xor_si2rm_v, cmp_si2rm_v)
 
 /* 0xc0 */
@@ -96,8 +96,8 @@ make_group(group7,
 helper_fun opcode_table [256] = {
 /* 0x00 */	add_r2rm_b, add_r2rm_v, add_rm2r_b, add_rm2r_v,
 /* 0x04 */	add_i2a_b, add_i2a_v, inv, inv,
-/* 0x08 */	inv, inv, inv, inv,
-/* 0x0c */	inv, inv, inv, _2byte_esc,
+/* 0x08 */	or_r2rm_b, or_r2rm_v, or_rm2r_b, or_rm2r_v,
+/* 0x0c */	or_i2a_b, or_i2a_v, inv, _2byte_esc,
 /* 0x10 */	adc_r2rm_b, adc_r2rm_v, adc_rm2r_b, adc_rm2r_v,
 /* 0x14 */	adc_i2a_b, adc_i2a_v, inv, inv,
 /* 0x18 */	inv, inv, inv, inv,
