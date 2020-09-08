b27819bb4e69450ddb3c8029696537bbe1bc8e98
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 13:45:20 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:45:20 up  5:12,  1 user,  load average: 0.58, 0.78, 0.55 169492e329651dd8b8d29e4170e0ed2ce1e749f9

---
+++ nemu/src/cpu/exec/lyh/movsw-template.h
@@ -0,0 +1,13 @@
+#include "cpu/exec/template-start.h"
+
+#define instr movsw
+
+static void do_execute() {
+	DATA_TYPE_S sv = (int8_t)(op_src->val & 0xffff);	//sign-extend
+	OPERAND_W(op_dest, sv);
+	print_asm_template2();
+}
+
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
---
+++ nemu/src/cpu/exec/lyh/movsw.h
@@ -0,0 +1,6 @@
+#ifndef __MOVSW_H__
+#define __MOVSW_H__
+
+make_helper(movsw_rm2r_v);
+
+#endif
