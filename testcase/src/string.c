5499f3681b257a8de8ae47621797b7c383650dfc
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 16:20:56 +0800

    >  run 3018210153 liyh Linux masterlyh 4.15.0-115-generic #116-Ubuntu SMP Wed Aug 26 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:20:56 up 56 min,  1 user,  load average: 1.42, 1.26, 1.08 66c48058d5e462f74621f04b7cc65e46aa74abe0

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -18,19 +18,19 @@ char str[20]="a";
 int main() {
 	//strcat(str, "H");
 	//nemu_assert(str[0]=='a');
-	nemu_assert(strcmp(s[0], s[2]) == 0);
-	nemu_assert(strcmp(s[0], s[1]) == -1);
-	nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
-	nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
-	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
-	strcat(str1, s[3]);
+	//nemu_assert(strcmp(s[0], s[2]) == 0);
+	//nemu_assert(strcmp(s[0], s[1]) == -1);
+	//nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
+	//nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
+	//nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
+	//strcat(str1, s[3]);
 	//set_bp();
 	nemu_assert(str1[0]=='H');
 	nemu_assert(strcmp(str1, s[4]) == 0);
 
-	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
+	//nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 
-	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
+	//nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
 	return 0;
 }
