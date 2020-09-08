727f7e06a6ddc460cf2250029c7dfe8573ed647a
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 14:26:35 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:26:35 up  5:54,  1 user,  load average: 1.47, 1.44, 1.12 632c2950fdd0d062d0be6b89cb01cca19cef9e24

--- nemu/src/cpu/exec/lyh/call-template.h
+++ nemu/src/cpu/exec/lyh/call-template.h
@@ -6,7 +6,7 @@ make_helper(concat(call_i_, SUFFIX)) {
 	int len = concat(decode_i_, SUFFIX)(eip + 1);
 	reg_l(R_ESP) -= DATA_BYTE;
 	MEM_W(reg_l(R_ESP), cpu.eip + len + 1);
-	cpu.eip += op_src->val;
+	cpu.eip += (DATA_TYPE_S)op_src->val;
 	print_asm("call 0x%x", cpu.eip + len + 1);
 	return len + 1;
 }
@@ -15,7 +15,7 @@ make_helper(concat(call_rm_, SUFFIX)) {
 	int len = concat(decode_rm_, SUFFIX)(eip + 1);
 	reg_l(R_ESP) -= DATA_BYTE;
 	MEM_W(reg_l(R_ESP), cpu.eip + len + 1);
-	cpu.eip = op_src->val - len - 1;
+	cpu.eip = (DATA_TYPE_S)op_src->val - len - 1;
 	print_asm("call %s", op_src->str);
 	return len + 1;
 }
--- nemu/src/cpu/exec/lyh/jge-template.h
+++ nemu/src/cpu/exec/lyh/jge-template.h
@@ -5,7 +5,7 @@
 make_helper(concat(jge_i_, SUFFIX)) {
 	int len = concat(decode_i_, SUFFIX) (eip + 1);
 	print_asm("jge 0x%x", (DATA_TYPE_S)op_src->imm + cpu.eip + len + 1);
-	if(cpu.SF == cpu.OF) cpu.eip += (DATA_TYPE_S)op_src->imm;
+	if(cpu.ZF == 1 || cpu.SF == cpu.OF) cpu.eip += (DATA_TYPE_S)op_src->imm;
 	return len + 1;
 }
 
