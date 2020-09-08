9eb565afbc61b98007f88391869638ccedd85d27
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 16:21:32 +0800

    >  run 3018210153 liyh Linux masterlyh 4.15.0-115-generic #116-Ubuntu SMP Wed Aug 26 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:21:32 up 56 min,  1 user,  load average: 1.15, 1.20, 1.06 1f7dd2cd13e8dc6a83365159b177e2cc5a926664

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -27,7 +27,7 @@ int main() {
 	//set_bp();
 	nemu_assert(strcmp(str1, s[4]) == 0);
 
-	//nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
+	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 
 	//nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
