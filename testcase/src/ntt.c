794e3151b36c4f72ad899e007abdfc64b75f19de
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 18:46:13 +0800

    >  run 3018210153 liyh Linux masterlyh 4.15.0-115-generic #116-Ubuntu SMP Wed Aug 26 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:46:13 up 36 min,  1 user,  load average: 1.04, 0.87, 0.74 ec68a306a6f8fab25acfd417594cb1ef908777c

--- testcase/src/ntt.c
+++ testcase/src/ntt.c
@@ -42,15 +42,15 @@ void NTT(int l, int *c, int type) {
     for (i = 0; i < l; i++)if (i < tr[i])swap(&c[i], &c[tr[i]]);
     for (mid = 1; mid < l; mid <<= 1) {
         int wn = qPow(G, (MOD - 1) / (mid << 1));
-        if (type == -1)wn = qPow(wn, MOD - 2);
+        if (type == -1)set_bp(), wn = qPow(wn, MOD - 2);
         for (len = mid << 1, j = 0; j < l; j += len) {
             int w = 1;
             for (k = 0; k < mid; k++, w = qMul(w, wn)) {
                 int x = c[j + k], y = qMul(w, c[j + mid + k]);
-				if(type == -1) {
-					set_bp();
-					nemu_assert(c[1] != 0);
-				}
+				//if(type == -1) {
+				//	set_bp();
+				//	nemu_assert(c[1] != 0);
+				//}
                 c[j + k] = (x + y) % MOD, c[j + mid + k] = (x - y) % MOD;
             }
         }
