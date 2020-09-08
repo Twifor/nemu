ff0084ccfb499b3b015dcc4c3b51da41d166647c
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 18:34:03 +0800

    >  run 3018210153 liyh Linux masterlyh 4.15.0-115-generic #116-Ubuntu SMP Wed Aug 26 14:04:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:34:03 up 24 min,  1 user,  load average: 0.89, 0.62, 0.67 c68172e874744dd3fb89c3c889f104e63309485

--- testcase/src/ntt.c
+++ testcase/src/ntt.c
@@ -42,8 +42,8 @@ void NTT(int l, int *c, int type) {
     for (i = 0; i < l; i++)if (i < tr[i])swap(&c[i], &c[tr[i]]);
     for (mid = 1; mid < l; mid <<= 1) {
         int wn = qPow(G, (MOD - 1) / (mid << 1));
-        if (type == -1)wn = qPow(wn, MOD - 2);
 		set_bp();
+        if (type == -1)wn = qPow(wn, MOD - 2);
         for (len = mid << 1, j = 0; j < l; j += len) {
             int w = 1;
             for (k = 0; k < mid; k++, w = qMul(w, wn)) {
