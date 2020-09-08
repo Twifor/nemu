d821e3dfd10beed6fc3f053ad615c897ac1e3a20
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-08 11:04:08 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:04:08 up  2:31,  1 user,  load average: 0.49, 0.91, 0.90 7930126944d7c2fd5baf4b2c37b0594f64211bee

--- nemu/src/cpu/exec/lyh/scas-template.h
+++ nemu/src/cpu/exec/lyh/scas-template.h
@@ -4,6 +4,7 @@
 
 make_helper(concat(scas_, SUFFIX)) {
 	swaddr_t s1 = REG(R_EAX), s2 = swaddr_read(REG(R_ESI), DATA_BYTE);
+	printf("%d %d\n", s1,s2);
 	uint32_t res = s1 - s2;
 	if (cpu.DF == 0) REG (R_ESI) += DATA_BYTE;
 	else REG (R_ESI) -= DATA_BYTE;
