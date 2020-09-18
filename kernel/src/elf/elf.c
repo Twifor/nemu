75f7dfda7fa98d6456ed9d3cf975b2075ecc4037
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-18 10:09:37 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:09:37 up  1:40,  1 user,  load average: 1.76, 1.44, 1.98 1581315f5ef559c6aef14a06b8dfac5925470c56

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -33,12 +33,11 @@ uint32_t loader() {
 	/* TODO: fix the magic number with the correct one */
 	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
-	set_bp();
 	nemu_assert(*p_magic == elf_magic);
-	set_bp();
-
 	/* Load each program segment */
-	panic("please implement me");
+	set_bp();
+	panic("please implement me");	
+	set_bp();
 	for(; true; ) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
