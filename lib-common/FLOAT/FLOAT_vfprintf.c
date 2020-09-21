0427b5822c6d9df1a845b7a63f033858d01804c7
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-21 16:17:15 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:17:15 up  2:09,  1 user,  load average: 2.54, 2.28, 2.12 7af16b84159428a9ffdf83ce641657e40630a94

--- lib-common/FLOAT/FLOAT_vfprintf.c
+++ lib-common/FLOAT/FLOAT_vfprintf.c
@@ -16,7 +16,7 @@ __attribute__((used)) static int format_FLOAT(FILE *stream, FLOAT f) {
 	 */
 
 	char buf[80];
-	int len = sprintf(buf, "0x%08x", f);
+	int len = (sprintf) (buf, "0x%08x", f);
 	return __stdio_fwrite(buf, len, stream);
 }
 
