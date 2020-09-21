562361972768acacf843dab4db50ac0cd6ed9ef4
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-21 15:18:55 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-117-generic #118-Ubuntu SMP Fri Sep 4 20:02:41 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:18:55 up  1:10,  1 user,  load average: 1.47, 1.76, 1.61 e7317fb59aa3320d7436144d3aa8d0213346848

--- nemu/src/cpu/exec/logic/shr-template.h
+++ nemu/src/cpu/exec/logic/shr-template.h
@@ -10,6 +10,8 @@ static void do_execute () {
 	dest >>= count;
 	OPERAND_W(op_dest, dest);
 
+	concat(updateCPU_, SUFFIX) (dest);
+
 	/* There is no need to update EFLAGS, since no other instructions 
 	 * in PA will test the flags updated by this instruction.
 	 */
