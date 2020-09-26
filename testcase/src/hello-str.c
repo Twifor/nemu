1f5516e7cc19abdc0474fd075d37f12c268718f3
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-26 19:41:50 +0800

    >  run 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:41:50 up  1:11,  1 user,  load average: 1.16, 1.01, 0.93 9b835f734b4b551d6acdc5e497fc6493c98a3511

--- testcase/src/hello-str.c
+++ testcase/src/hello-str.c
@@ -10,11 +10,11 @@ int main() {
 	//set_bp();
 	nemu_assert(strcmp(buf, "Hello world!\n") == 0);
 
-	//sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
-	//nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
+	sprintf(buf, "%d + %d = %d\n", 1, 1, 2);
+	nemu_assert(strcmp(buf, "1 + 1 = 2\n") == 0);
 
-	//sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
-	//nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
+	sprintf(buf, "%d + %d = %d\n", 2, 10, 12);
+	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
 
 	return 0;
 }
