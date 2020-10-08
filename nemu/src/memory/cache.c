0f9e7ac0e57bc1da0ff00df7f002509f8a417fba
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-08 20:46:18 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:46:18 up  2:50,  1 user,  load average: 0.88, 0.93, 0.85 bd42f4eaa8172a7933acbc9ea5996cf2932b2ca6

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -22,15 +22,24 @@ void resetCache() {
 int readCache(hwaddr_t addr) {
 	uint32_t tag = addr >> (CACHE_BLOCK_SIZE_BIT + CACHE_SET_BIT);
 	uint32_t set = (addr >> CACHE_BLOCK_SIZE_BIT) & (CACHE_SET_SIZE - 1);
+	uint32_t block = (addr >> CACHE_BLOCK_SIZE_BIT) << CACHE_BLOCK_SIZE_BIT;
 	//not offset
 	int i = 0, j;
 	for(i = CACHE_WAY_SIZE * set; i < CACHE_WAY_SIZE * (set + 1); i++) {
 		if(cache[i].tag == tag && cache[i].valid) return i;
 	}
+/*
 	j = readCache2(addr);
 	srand(i);
 	i = CACHE_WAY_SIZE * set + rand() % CACHE_WAY_SIZE;//random
 	memcpy(cache[i].data, cache2[j].data, CACHE_BLOCK_SIZE);//CACHE2_BLOCK_SIZE=CACHE_BLOCK_SIZE
+*/
+	srand(i);
+	i = CACHE_WAY_SIZE * set + rand() % CACHE_WAY_SIZE;//random
+	for(j = 0; j < CACHE_BLOCK_SIZE / BURST_LEN; j++) {
+		ddr3_read_public(block + j * BURST_LEN , cache[i].data + j * BURST_LEN);
+	}
+
 	cache[i].valid = true;
 	cache[i].tag = tag;
 	return i;
@@ -70,20 +79,20 @@ void writeCache(hwaddr_t addr, size_t len, uint32_t data) {
 	for(i = CACHE_WAY_SIZE * set; i < CACHE_WAY_SIZE * (set + 1); i++) {
 		if(cache[i].tag == tag && cache[i].valid) {
 			if(offset + len > CACHE_BLOCK_SIZE) {//across
-				dram_write(addr, CACHE_BLOCK_SIZE - offset, data);	//write through
+				//dram_write(addr, CACHE_BLOCK_SIZE - offset, data);	//write through
 				memcpy(cache[i].data + offset, &data, CACHE_BLOCK_SIZE - offset);
-				writeCache2(addr, CACHE_BLOCK_SIZE - offset, data);//update cache2
+				//writeCache2(addr, CACHE_BLOCK_SIZE - offset, data);//update cache2
 				writeCache(addr + CACHE_BLOCK_SIZE - offset, len - CACHE_BLOCK_SIZE + offset, data >> (CACHE_BLOCK_SIZE - offset));
 			} else {
-				dram_write(addr, len, data);
+				//dram_write(addr, len, data);
 				memcpy(cache[i].data + offset, &data, len);
-				writeCache2(addr, len, data);
+				//writeCache2(addr, len, data);
 			}
 			return;
 		}
 	}
 	//fail. write cache2
-	writeCache2(addr, len, data);
+	//writeCache2(addr, len, data);
 }
 
 void writeCache2(hwaddr_t addr, size_t len, uint32_t data) {
