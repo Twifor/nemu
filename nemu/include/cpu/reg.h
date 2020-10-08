cdc8e3d6547ffdf93d5b03744672efce36601f75
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-09 00:26:19 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  00:26:19 up  1:50,  1 user,  load average: 1.04, 0.90, 0.98 edc8ec922dca1439b1a8125019d40df2bdc1aaa5

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -61,13 +61,13 @@ typedef struct {
 	};
 	union {
 		struct {
-			uint32_t PG : 1;
-			uint32_t UNDEFINED : 26;
-			uint32_t ET : 1;
-			uint32_t TS : 1;
-			uint32_t EM : 1;
-			uint32_t MP : 1;
 			uint32_t PE : 1;
+			uint32_t MP : 1;
+			uint32_t EM : 1;
+			uint32_t TS : 1;
+			uint32_t ET : 1;
+			uint32_t UNDEFINED : 26;
+			uint32_t PG : 1;
 		};
 		uint32_t cr0;
 	};
