d8203d306772b87ab645638ecea681a4efd9aae7
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-08 20:57:19 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:57:19 up  3:01,  1 user,  load average: 0.98, 0.84, 0.82 7c9831d51cb9740e260c98e02a930ffb6a34adf3

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -120,6 +120,5 @@ void writeCache2(hwaddr_t addr, size_t len, uint32_t data) {
 	i = readCache2(addr);
 	cache2[i].dirty = true;
 	memcpy(cache2[i].data + offset, &data, len);
-	printf("%d %d\n",(int)offset,(int)len);
-	assert(offset + len < CACHE2_BLOCK_SIZE);
+	assert(offset + len <= CACHE2_BLOCK_SIZE);
 }
