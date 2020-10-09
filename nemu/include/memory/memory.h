1c1d45b6541691e5abd68445566f2e53fec4f503
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-09 16:00:43 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:00:43 up  2:46,  1 user,  load average: 1.84, 1.28, 1.00 9ad46eafb510082b149bfdb83a4f588f72f66b90

--- nemu/include/memory/memory.h
+++ nemu/include/memory/memory.h
@@ -24,4 +24,6 @@ void swaddr_write(swaddr_t, size_t, uint32_t, uint8_t);
 void lnaddr_write(lnaddr_t, size_t, uint32_t);
 void hwaddr_write(hwaddr_t, size_t, uint32_t);
 
+void loadSregCache(uint8_t sreg);
+
 #endif
