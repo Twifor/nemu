6957ce43f4ccbde3979df2e20c38a6b68b1bcf4b
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-15 20:58:27 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:58:27 up  2:06,  1 user,  load average: 1.45, 1.23, 1.11 37ff00716972a6634172b7e7221463371c01748e

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -13,7 +13,11 @@ void ramdisk_read(uint8_t *, uint32_t, uint32_t);
 
 #define STACK_SIZE (1 << 20)
 
-void create_video_mapping();
+void create_video_mapping() {
+
+
+}
+
 uint32_t get_ucr3();
 
 uint32_t loader() {
@@ -71,9 +75,9 @@ uint32_t loader() {
 #ifdef HAS_DEVICE
 	create_video_mapping();
 #endif
-	set_bp();
+
 	write_cr3(get_ucr3());
-	set_bp();
+
 #endif
 
 	return entry;
