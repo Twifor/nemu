2eeb075704a60c6fe1aa38dd7b4c303432a1b533
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-15 18:31:19 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:31:19 up  3:54,  1 user,  load average: 0.66, 0.75, 0.70 9fbd00297ff4b6aaf35f1ffce51939f8f2bf464b

--- nemu/src/cpu/exec/lyh/jge-template.h
+++ nemu/src/cpu/exec/lyh/jge-template.h
@@ -6,7 +6,7 @@ make_helper(concat(jge_i_, SUFFIX)) {
 	int len = concat(decode_i_, SUFFIX) (eip + 1);
 	print_asm("jge 0x%x", (DATA_TYPE_S)op_src->imm + cpu.eip + len + 1);
 	if(cpu.ZF == 1 || cpu.SF == cpu.OF) cpu.eip += (DATA_TYPE_S)op_src->imm;
-	printf("%d\n",len+1);
+	printf("%d %d\n",len+1,eip);
 	return len + 1;
 }
 
