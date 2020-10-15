3f4da1c0eae8cf287cd37e45d5ac045726a73afc
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-15 21:33:58 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:33:58 up  2:42,  1 user,  load average: 0.86, 1.04, 0.99 48c9ee174f82edb22c55fcdf26e3bb5bcd5308f5

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -38,7 +38,7 @@ hwaddr_t page_translate(lnaddr_t addr, size_t len) {
 		uint32_t page_offset = ((addr >> 12) & 0x3ff);
 		uint32_t offset = addr & 0xfff;
 		dir.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + (dir_offset << 2), 4);
-		printf("%x %x\n",dir_offset,page_offset);
+		printf("%x %x %x\n",dir_offset,page_offset,offset);
 		Assert(dir.p, "Invalid page.");
 		page.val = hwaddr_read((dir.base << 12) + (page_offset << 2), 4);
 		Assert(page.p, "Invalid page.");
