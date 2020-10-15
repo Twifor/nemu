c036c284fd6133587b768ee2018b4daa5ccdf13f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-15 19:30:28 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:30:28 up 38 min,  1 user,  load average: 1.30, 0.98, 0.89 c76b69bf7756bf8b1b378f646907a57e1f3d969a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -65,7 +65,7 @@ static int cmd_info(char *args){
 		}
 	} else if(opt == 'c') {
 		printf("%%cr0 0x%x\n", cpu.cr0);
-		printf("%%cr3 0x%x 0x%x\n", cpu.cr3, cpu.UNDEFINDED2);
+		printf("%%cr3 0x%x 0x%x 0x%x 0x%x\n", cpu.cr3, cpu.UNDEFINDED2,cpu.PCD,cpu.PWT);
 	}
 	return 0;
 }
