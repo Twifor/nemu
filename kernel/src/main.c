312ca25cd17fc70ae94a4735e143d41d9cc07a85
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-20 11:23:00 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:23:00 up 12 min,  1 user,  load average: 1.00, 0.84, 0.65 8587b1ae41a4ec2267114e4a17442cb47c598152

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -77,6 +77,7 @@ void init_cond() {
 	 */
 	Log("Hello, NEMU world!");
 
+//In PA3-3
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
 	video_mapping_write_test();
