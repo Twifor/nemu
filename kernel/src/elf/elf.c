70b71b91073c7e6790a84cb72a130184395a3c12
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-21 17:31:32 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-121-generic #123-Ubuntu SMP Mon Oct 5 16:16:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:31:32 up 11 min,  1 user,  load average: 0.72, 1.45, 1.16 b50d08dafa32adcb210ff394eed2148ae29f84d3

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -44,6 +44,7 @@ uint32_t loader() {
 	for(; i < elf->e_phnum; i++, ph++) {
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
+			set_bp();
 			ph->p_vaddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
