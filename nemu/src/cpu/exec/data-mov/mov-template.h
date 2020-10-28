831b30c49f82dc06a409ad3ddfff74e21bef9c10
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 20:45:23 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:45:23 up  1:42,  1 user,  load average: 1.59, 1.34, 1.06 60ad581db8d2517da2d75c21497d300dcf541ad6

--- nemu/src/cpu/exec/data-mov/mov-template.h
+++ nemu/src/cpu/exec/data-mov/mov-template.h
@@ -50,6 +50,7 @@ make_helper(mov_r2cr) {
 		print_asm("mov %%%s,%%cr0", REG_NAME(R_EAX));
 	} else if(opcode == 0xd8) {
 		cpu.cr3.val = cpu.eax;
+		printf("xxx %x\n",cpu.eax);
 		resetTLB();
 		print_asm("mov %%%s,%%cr3", REG_NAME(R_EAX));
 	}
