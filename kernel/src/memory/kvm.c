843c49de75a54fe80ad94e49f44c9c7193496a1b
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 20:21:24 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:21:24 up  1:18,  1 user,  load average: 1.22, 0.68, 0.67 1787bbdf6489f441388c5baaaed8a7bdb75b3

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -60,7 +60,7 @@ void init_page(void) {
 	cr0.val = read_cr0();
 	cr0.paging = 1;
 	write_cr0(cr0.val);
-	//set_bp();
+	set_bp();
 }
 
 /* GDT in the kernel's memory, whose virtual memory is greater than 0xC0000000. */
