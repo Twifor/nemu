eb9b1635bca008b3867a518bf3668a95993b0072
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 22:36:28 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:36:28 up  1:13,  1 user,  load average: 1.77, 1.26, 1.07 a92dc582cc58f854b9712ecdc755051b7bcac73

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -21,8 +21,8 @@ void init_page(void) {
 
 	/* fill PDEs */
 	for (pdir_idx = 0; pdir_idx < PHY_MEM / PT_SIZE; pdir_idx ++) {
-		//pdir[pdir_idx].val = make_pde(ptable);
-		pdir[pdir_idx + KOFFSET / PT_SIZE].val = make_pde(ptable);
+		pdir[pdir_idx].val = make_pde(ptable);//eip
+		pdir[pdir_idx + KOFFSET / PT_SIZE].val = make_pde(ptable);//esp
 
 		ptable += NR_PTE;
 	}
