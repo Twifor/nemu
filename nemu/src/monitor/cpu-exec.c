90ae944a4f8241d049e9780054d013928f417239
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 20:45:40 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:45:40 up  1:42,  1 user,  load average: 1.68, 1.38, 1.08 6207c068c7f7df22614655f1e02535f0ea56a2f4

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -64,11 +64,11 @@ void cpu_exec(volatile uint32_t n) {
 		int instr_len = exec(cpu.eip);
 		cpu.eip += instr_len;
 
-		printf("xxx\n");
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len);
 		strcat(asm_buf, assembly);
 		Log_write("%s\n", asm_buf);
+		printf("xxx\n");
 		if(n_temp < MAX_INSTR_TO_PRINT) {
 			printf("%s\n", asm_buf);
 		}
