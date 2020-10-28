e3403e0d150ca4fd4b8f0e06e9ff63a53d592909
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 19:13:00 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:13:00 up 9 min,  1 user,  load average: 1.21, 1.15, 0.71 f7028d3bb278b1426c09b6d92eab7232502baa7

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -24,6 +24,7 @@ void init() {
 #ifdef IA32_PAGE
 	int x;
 	x=1;
+	set_bp();
 	/* We must set up kernel virtual memory first because our kernel thinks it 
 	 * is located at 0xc0100000, which is set by the linking options in Makefile.
 	 * Before setting up correct paging, no global variable can be used. */
