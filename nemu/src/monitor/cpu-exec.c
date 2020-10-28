25e4d24f38777e9a23eeb412e0a2bca404e77913
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-28 20:44:01 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:44:01 up  1:40,  1 user,  load average: 2.62, 1.37, 1.04 7c9a9ea7252afcb3d904ce4e785d09b54f89fca

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -61,6 +61,7 @@ void cpu_exec(volatile uint32_t n) {
 
 		/* Execute one instruction, including instruction fetch,
 		 * instruction decode, and the actual execution. */
+		printf("xxx\n");
 		int instr_len = exec(cpu.eip);
 		cpu.eip += instr_len;
 
