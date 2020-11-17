f083d24503ce410ed92a70de3d3dbfc58e7952da
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-17 21:09:01 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:09:01 up  3:08,  1 user,  load average: 0.73, 0.77, 1.06 8cea3f50d3875094fd8ceefa75d2ff37500e676e

--- nemu/src/cpu/exec/lyh/PA4/iret-template.h
+++ nemu/src/cpu/exec/lyh/PA4/iret-template.h
@@ -3,7 +3,7 @@
 DATA_TYPE concat(tmp_pop_, SUFFIX)();
 
 make_helper(concat(iret_, SUFFIX)) {
-	cpu.eip = concat(tmp_pop_, SUFFIX) ();
+	cpu.eip = concat(tmp_pop_, SUFFIX) () - 1;//guess why?
 	cpu.cs.val = concat(tmp_pop_, SUFFIX) ();
 	cpu.eflags = concat(tmp_pop_, SUFFIX) ();
 	if(cpu.cr0.protect_enable) loadSregCache(R_CS);
