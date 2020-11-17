9f0070c91e60c4a294e058b190b59786652b9c16
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-17 19:50:49 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:50:49 up  1:49,  1 user,  load average: 0.64, 0.92, 1.01 53a2e0056bf8d38030083523598f2fdca991ded

--- nemu/src/cpu/exec/lyh/PA4/int-template.h
+++ nemu/src/cpu/exec/lyh/PA4/int-template.h
@@ -4,10 +4,11 @@ void raise_intr(uint8_t NO);
 
 make_helper(my_int) {
 	uint8_t NO = instr_fetch(eip + 1, 1);
+	cpu.eip += 2;
 	print_asm("int　%x", NO);
 	raise_intr(NO);
 
-	return 2;
+	return 0;
 }
 
 #include "cpu/exec/template-end.h"
