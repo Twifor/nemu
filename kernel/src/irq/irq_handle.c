429d7895fbe50344eb83c342ec51af1d53c8f1fa
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-17 20:37:36 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:37:36 up  2:36,  1 user,  load average: 1.53, 2.11, 2.37 2e6b44592fbbec1d6ede963eb4be6c12a11da9ef

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -36,9 +36,8 @@ void irq_handle(TrapFrame *tf) {
 	//panic("Have you re-organized the `TrapFrame' structure?");
 
 	int irq = tf->irq;
-
+	set_bp();
 	if (irq < 0) {
-		set_bp();
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
 		do_syscall(tf);
