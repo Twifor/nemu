395017aa69d8464db4e269b7c8f6f003d3e54f90
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-17 20:37:54 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:37:54 up  2:36,  1 user,  load average: 1.46, 2.05, 2.35 cd165bbd984f1afd39af7ce4560da21da3e002ef

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -42,6 +42,7 @@ void irq_handle(TrapFrame *tf) {
 	} else if (irq == 0x80) {
 		do_syscall(tf);
 	} else if (irq < 1000) {
+		set_bp();
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
 	} else if (irq >= 1000) {
 		int irq_id = irq - 1000;
