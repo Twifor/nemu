7b8deccf75f852f613066d799f4bafa33402b2e8
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-17 19:59:43 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:59:43 up  1:58,  1 user,  load average: 1.01, 0.88, 0.95 2f04c8b25f7004fcfe3c67d78bf4dc820c01e68

--- nemu/src/cpu/exec/lyh/PA4/intr.c
+++ nemu/src/cpu/exec/lyh/PA4/intr.c
@@ -9,6 +9,11 @@ void raise_intr(uint8_t NO) {
 	printf("int %x\n", NO);//debug use
 
 	tmp_push_l(cpu.eflags);
+	tmp_push_l(cpu.cs.val);
+	tmp_push_l(cpu.eip);
+
+	uint32_t base_addr = cpu.idtr.base_addr;
+	printf("%x\n",base_addr);
 	
 	//Interrupt start
 	
