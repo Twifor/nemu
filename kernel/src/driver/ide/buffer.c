178279ed89b72a9cf11e1e8123de86a239343033
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 17:24:44 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:24:44 up  2:08,  1 user,  load average: 0.76, 0.97, 0.95 ee816e5eb4aabce24a1ca75f7da3d07bc31c88f

--- kernel/src/driver/ide/buffer.c
+++ kernel/src/driver/ide/buffer.c
@@ -55,6 +55,7 @@ uint8_t
 read_byte(uint32_t offset) {
 	uint32_t sector = offset >> 9;
 	struct SectorBuf *ptr = buf_fetch(sector);
+	printk("%x %x\n",sector, offset);
 	return ptr->content[offset & 511];
 }
 
