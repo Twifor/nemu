4a9611343537f36601061dd1a4ac78fecc32db80
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 11:10:44 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:10:44 up 41 min,  1 user,  load average: 0.93, 1.37, 1.35 4d5f75c2bc20194a601cafc77c34306ffc6f24f2

---
+++ nemu/src/cpu/exec/lyh/PA4/out.c
@@ -0,0 +1,16 @@
+#include "cpu/exec/helper.h"
+
+#define DATA_BYTE 1
+#include "out-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "out-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "out-template.h"
+#undef DATA_BYTE
+
+make_helper_v(out_i2a)
+make_helper_v(out_r2rm)
