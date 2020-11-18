1ff13b00dc609932a3b70801af5d9eab95c9e463
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 20:11:17 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:11:17 up  4:54,  1 user,  load average: 0.94, 1.07, 1.10 a8d0a3d11fa22fe81a09760e800037e2bef2bf59

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -32,13 +32,13 @@ void do_syscall(TrapFrame *tf) {
 		
 		case SYS_write://SYS_WRITE
 			{
-				/*if(tf->ecx == 0x80480aa) {
-					set_bp();
-					nemu_assert(tf->edx == 0xe);
-				}*/
+#ifdef HAS_DEVICE
 				void serial_printc(char ch);
 				int i = 0;
 				for(i = 0; i < tf->edx; i++) serial_printc(*((char *)((char *)tf->ecx + i)));
+#else
+				asm volatile(".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
+#endif
 				tf->eax = tf->edx;
 		   		break;
 			}
