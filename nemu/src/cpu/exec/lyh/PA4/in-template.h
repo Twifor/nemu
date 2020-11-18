a7437fa9d4a0d45ef0ef38beaff087380716b297
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 20:52:47 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:52:47 up  5:36,  1 user,  load average: 1.43, 1.41, 1.27 f274c0d6aa79a486ec40493dc5c394424b23958d

--- nemu/src/cpu/exec/lyh/PA4/in-template.h
+++ nemu/src/cpu/exec/lyh/PA4/in-template.h
@@ -7,14 +7,14 @@ uint32_t pio_read(ioaddr_t, size_t);
 make_helper(concat(in_i2a_, SUFFIX)) {
 	uint8_t imm = instr_fetch(eip + 1, 1);
 	concat(reg_, SUFFIX)(R_EAX) = pio_read(imm, DATA_BYTE);
-//	printf("in  (%d->%x) %x\n", DATA_BYTE, imm, concat(reg_, SUFFIX)(R_EAX));
+	printf("in  (%d->%x) %x\n", DATA_BYTE, imm, concat(reg_, SUFFIX)(R_EAX));
 	print_asm("in %x", imm);
 	return 2;
 }
 
 make_helper(concat(in_, SUFFIX)) {
 	concat(reg_, SUFFIX)(R_EAX) = pio_read(reg_w(R_DX), DATA_BYTE);
-//	printf("in  (%d->%x) %x\n", DATA_BYTE, reg_w(R_DX), concat(reg_, SUFFIX)(R_EAX));
+	printf("in  (%d->%x) %x\n", DATA_BYTE, reg_w(R_DX), concat(reg_, SUFFIX)(R_EAX));
 	print_asm("in");
 	return 1;
 }
--- nemu/src/cpu/exec/lyh/PA4/out-template.h
+++ nemu/src/cpu/exec/lyh/PA4/out-template.h
@@ -7,14 +7,14 @@ uint32_t pio_write(ioaddr_t, size_t,  uint32_t);
 make_helper(concat(out_i2a_, SUFFIX)) {
 	uint8_t imm = instr_fetch(eip + 1, 1);
 	pio_write(imm, DATA_BYTE, (DATA_TYPE)reg_l(R_EAX));
-	//printf("out  (%d->%x) %x\n", DATA_BYTE,imm, concat(reg_, SUFFIX)(R_EAX));
+	printf("out  (%d->%x) %x\n", DATA_BYTE,imm, concat(reg_, SUFFIX)(R_EAX));
 	print_asm("out %x", imm);
 	return 2;
 }
 
 make_helper(concat(out_, SUFFIX)) {
 	pio_write(reg_w(R_DX), DATA_BYTE, (DATA_TYPE)reg_l(R_EAX));
-	//printf("out  (%d->%x) %x\n", DATA_BYTE,reg_w(R_DX), concat(reg_, SUFFIX)(R_EAX));
+	printf("out  (%d->%x) %x\n", DATA_BYTE,reg_w(R_DX), concat(reg_, SUFFIX)(R_EAX));
 	print_asm("out");
 	return 1;
 }
