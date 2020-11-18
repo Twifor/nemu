ab032bca61cfcffdf7a4ed575152995bb7c4daf5
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 11:50:45 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:50:45 up  1:21,  1 user,  load average: 1.21, 1.57, 1.58 d77dda24ff6d15326b003d9e2e028c48115647c7

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -44,6 +44,7 @@ void init() {
 
 /* Initialization phase 2 */
 void init_cond() {
+	set_bp();
 #ifdef IA32_INTR
 	/* Reset the GDT, since the old GDT in start.S cannot be used in the future. */
 	init_segment();
@@ -61,8 +62,7 @@ void init_cond() {
 
 	/* Initialize the serial port. After that, you can use printk() to output messages. */
 	init_serial();
-	set_bp();
-	Log("success");
+	//Log("success");
 
 	/* Initialize the IDE driver. */
 	init_ide();
