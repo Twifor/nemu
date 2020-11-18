83df2e02e84ba70b7bea1ee01fedcaa6a4b8496d
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 16:15:47 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:15:47 up 59 min,  1 user,  load average: 0.92, 0.72, 0.65 9d76c7bdc55bd274ab662f997d893d6bcfdd59

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -95,6 +95,7 @@ void init_cond() {
 #endif
 	/* Load the program. */
 	uint32_t eip = loader();
+	set_bp();
 	
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
