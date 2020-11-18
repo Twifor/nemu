34b016357a6495085c5b48f1202bc7182c2668db
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 16:46:32 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:46:32 up  1:29,  1 user,  load average: 1.50, 0.96, 0.86 279cacaf3af837652cc3b920b3eae818955ac1be

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -66,7 +66,7 @@ void init_cond() {
 
 	/* Initialize the serial port. After that, you can use printk() to output messages. */
 	init_serial();
-	//Log("success");
+	printk("hello\n");
 
 	/* Initialize the IDE driver. */
 	init_ide();
