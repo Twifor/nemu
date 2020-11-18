8f580fb0838170d620800c4e79bd686016aeab87
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 22:44:38 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  22:44:37 up  7:28,  1 user,  load average: 1.08, 1.45, 1.37 aa93e68899b5a89c4eaa8766454d4c8e2464cf8

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -15,14 +15,18 @@ static void sys_ioctl(TrapFrame *tf) {
 }
 
 static void sys_write(TrapFrame *tf) {
-#ifdef HAS_DEVICE
-	void serial_printc(char ch);
-	int i = 0;
-	for(i = 0; i < tf->edx; i++) serial_printc(*((char *)((char *)tf->ecx + i)));
-#else
-	asm volatile(".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
-#endif
+	if(tf->ebx == 1 || tf->ebx == 2) {
+	#ifdef HAS_DEVICE
+		void serial_printc(char ch);
+		int i = 0;
+		for(i = 0; i < tf->edx; i++) serial_printc(*((char *)((char *)tf->ecx + i)));
+	#else
+		asm volatile(".byte 0xd6" : : "a"(2), "c"(tf->ecx), "d"(tf->edx));
+	#endif
 	tf->eax = tf->edx;
+	} else {
+		panic("Unhandled system call: id = %d, eip = 0x%08x", tf->eax, tf->eip);
+	}
 }
 
 void do_syscall(TrapFrame *tf) {
