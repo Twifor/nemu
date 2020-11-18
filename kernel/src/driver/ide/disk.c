0da5411fa03c3b09492c02c94d998d31b48a2649
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 17:50:33 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:50:33 up  2:33,  1 user,  load average: 1.74, 1.32, 1.14 f4b08afd97d001ca784f7856680a18ef159e9430

--- kernel/src/driver/ide/disk.c
+++ kernel/src/driver/ide/disk.c
@@ -65,7 +65,7 @@ disk_do_read(void *buf, uint32_t sector) {
 	//set_bp();
 	for (i = 0; i < 512 / sizeof(uint32_t); i ++) {
 		*(((uint32_t*)buf) + i) = in_long(IDE_PORT_BASE);
-		printk("\n%x\n",*(((uint32_t*)buf) + 0x2e));
+		if(i>=0x2e && i<= 0x30)printk("\n%x\n",*(((uint32_t*)buf) + 0x2e));
 	//	printk("\n%x %x\n",i*4,*(((uint32_t*)buf) + i));
 	}
 	printk("%x %x %x",*(((uint32_t*)buf) + 0x2d), *(((uint32_t*)buf) + 0x2e), *(((uint32_t*)buf) + 0x2f));
