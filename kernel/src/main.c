774f57fc2ced33120acce8d90fbff7b7837a485b
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 12:14:56 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:14:56 up  1:46,  1 user,  load average: 1.57, 1.52, 1.45 5d864416e2965d27aaf7afe2b83dfb4748a91c60

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -90,6 +90,7 @@ void init_cond() {
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Write some test data to the video memory. */
 	video_mapping_write_test();
+	set_bp();
 #endif
 	/* Load the program. */
 	uint32_t eip = loader();
