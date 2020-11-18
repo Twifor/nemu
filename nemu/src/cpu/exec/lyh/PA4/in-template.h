50926ffcd52e9d60a996a5a8da3c74068ec6573e
tracer-NEMU2020 <tjuics2020@163.com>
2020-11-18 17:14:32 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-123-generic #126-Ubuntu SMP Wed Oct 21 09:40:11 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:14:32 up  1:57,  1 user,  load average: 0.85, 1.04, 0.97 ba25cfe8b86a2e1046a2e5b15039fe4bf9f3c2b

--- nemu/src/cpu/exec/lyh/PA4/in-template.h
+++ nemu/src/cpu/exec/lyh/PA4/in-template.h
@@ -7,13 +7,13 @@ uint32_t pio_read(ioaddr_t, size_t);
 make_helper(concat(in_i2a_, SUFFIX)) {
 	uint8_t imm = instr_fetch(eip + 1, 1);
 	concat(reg_, SUFFIX)(R_EAX) = pio_read(imm, DATA_BYTE);
-	printf("in %x: %x\n",imm, concat(reg_, SUFFIX)(R_EAX));
 	print_asm("in %x", imm);
 	return 2;
 }
 
 make_helper(concat(in_, SUFFIX)) {
 	concat(reg_, SUFFIX)(R_EAX) = pio_read(reg_w(R_DX), DATA_BYTE);
+	printf("in  %x\n",concat(reg_, SUFFIX)(R_EAX));
 	print_asm("in");
 	return 1;
 }
