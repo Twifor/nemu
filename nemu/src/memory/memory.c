d06f13ebd38bfeb37e73c9b7c4c81f4878c1b980
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 15:50:42 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:50:41 up 5 min,  1 user,  load average: 3.32, 2.72, 1.29 c02ca5dc60d6af9bbcfdd0825683eef970dc4d7

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -50,29 +50,29 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {	//physical address
 hwaddr_t page_translate(lnaddr_t addr, size_t len) {
 	if(cpu.cr0.protect_enable && cpu.cr0.paging) {
 	//	printf("%x\n",addr);
-		//hwaddr_t tmpad;
-		//if((tmpad = readTLB(addr & 0xfffff000)) != -1) return (tmpad << 12) + (addr & 0xfff);
+		hwaddr_t tmpad;
+		if((tmpad = readTLB(addr & 0xfffff000)) != -1) return (tmpad << 12) + (addr & 0xfff);
 		PageEntry dir, page;
 		uint32_t dir_offset = addr >> 22;
 		uint32_t page_offset = ((addr >> 12) & 0x3ff);
 		uint32_t offset = addr & 0xfff;
 		dir.val = hwaddr_read((cpu.cr3.page_directory_base << 12) + (dir_offset << 2), 4);
-		/*if(!dir.p) {
+		if(!dir.p) {
 			printf("Invalid page\n");
 			void do_int3();
 			do_int3();//debug use
-		}*/
+		}
 		Assert(dir.p, "Invalid page. %x", addr);
 		page.val = hwaddr_read((dir.base << 12) + (page_offset << 2), 4);
-		/*if(!page.p) {
+		if(!page.p) {
 			printf("Invalid page\n");
 			void do_int3();
 			do_int3();//debug use
-		}*/
+		}
 		Assert(page.p, "Invalid page. %x", addr);
-	//	hwaddr_t hwaddr = (page.base << 12) + offset;
-		//Assert((hwaddr & 0xfff) + len == ((hwaddr + len) & 0xfff), "Fatal Error!!");
-		//writeTLB(addr & 0xfffff000, page.base);
+		hwaddr_t hwaddr = (page.base << 12) + offset;
+		Assert((hwaddr & 0xfff) + len == ((hwaddr + len) & 0xfff), "Fatal Error!!");
+		writeTLB(addr & 0xfffff000, page.base);
 		return (page.base << 12) + offset;
 	} else {
 		return addr;
