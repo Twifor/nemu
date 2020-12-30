76c7ade1f8ce832da04926f23f41c23e3667957c
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 16:36:05 +0800

    >  compile kernel 3018210153 liyh Linux masterlyh 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:36:05 up 51 min,  1 user,  load average: 2.04, 2.24, 2.00 cc3eadb4ca4fba40f0441764fa0e867e96a3f749

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -68,6 +68,7 @@ uint32_t loader() {
 #endif
 		}
 	}
+	set_bp();
 	volatile uint32_t entry = elf->e_entry;
 
 #ifdef IA32_PAGE
--- kernel/src/main.c
+++ kernel/src/main.c
@@ -104,7 +104,7 @@ void init_cond() {
 	/* Load the program. */
 	Log("Loading...");
 	uint32_t eip = loader();
-	set_bp();
+//	set_bp();
 	
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
