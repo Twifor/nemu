885e1a0b581779cc268d747253025657f15ff492
tracer-NEMU2020 <tjuics2020@163.com>
2020-12-30 17:00:50 +0800

    >  compile NEMU 3018210153 liyh Linux masterlyh 4.15.0-128-generic #131-Ubuntu SMP Wed Dec 9 06:57:35 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:00:50 up  1:16,  1 user,  load average: 1.68, 1.68, 1.79 488229ff13c40e1980eb3ad2b3b50c18e60b125

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -59,12 +59,15 @@ int readCache2(hwaddr_t addr) {
 	srand(i);
 	i = CACHE2_WAY_SIZE * set + rand() % CACHE2_WAY_SIZE;//random
 	if(cache2[i].dirty && cache2[i].valid) {//write back
-		uint8_t mask[BURST_LEN * 2];
+		//uint8_t mask[BURST_LEN * 2];
 		uint32_t block2 = (cache2[i].tag << (CACHE2_BLOCK_SIZE_BIT + CACHE2_SET_BIT)) | (set << CACHE2_BLOCK_SIZE_BIT);
-		memset(mask, 1, BURST_LEN * 2);
-		for (j = 0; j < CACHE2_BLOCK_SIZE / BURST_LEN; j++) {
-			ddr3_write_public(block2 + j * BURST_LEN, cache2[i].data + j * BURST_LEN, mask);
+		for(j = 0; j < (CACHE_BLOCK_SIZE >> 2); j++) {
+			dram_write(block2 + (j << 2), 4, *((uint32_t *)(cache2[i].data + (j << 2))));
 		}
+		//memset(mask, 1, BURST_LEN * 2);
+		//for (j = 0; j < CACHE2_BLOCK_SIZE / BURST_LEN; j++) {
+		//	ddr3_write_public(block2 + j * BURST_LEN, cache2[i].data + j * BURST_LEN, mask);
+		//}
 	}
 	for(j = 0; j < CACHE2_BLOCK_SIZE / BURST_LEN; j++) {
 		ddr3_read_public(block + j * BURST_LEN , cache2[i].data + j * BURST_LEN);
